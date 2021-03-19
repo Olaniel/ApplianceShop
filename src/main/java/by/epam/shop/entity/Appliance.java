4f43c3494f6cb0483754eeb27f0e7bf20aeaed7f
Olaniel <olaniel@mail.ru>
2021-03-19 05:51:21 +0300

    Update Appliance.java

--- src/main/java/by/epam/shop/entity/Appliance.java
+++ src/main/java/by/epam/shop/entity/Appliance.java
@@ -2,7 +2,7 @@ package by.epam.shop.entity;
 
 import java.util.List;
 
-public class Appliance {
+public class Appliance {// вкорне неверно построенная иерархия сущностей. Мы на код-ревью разбирали правила, и наследованиеее
     List<String> applianceList;
 
     @Override
